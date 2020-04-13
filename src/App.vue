148f845b280fc7354007787f4db6b226031806fd
Jakob Andersson <erikjakobandersson@gmail.com>
2020-04-13 13:57:45 +0200

    Add FooterBar.

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
   <div id="app">
     <HeaderBar navigationTitle="Jakob Andersson"/>
     <body>
+      <!-- replace with component -->
       <p>This is in App</p>
       <p>Lipsum lipsum etc.</p>
     </body>
+    <FooterBar />
   </div>
 </template>
 
 <script>
 import HeaderBar from './components/HeaderBar.vue'
+import FooterBar from './components/FooterBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderBar
+    HeaderBar,
+    FooterBar
   }
 }
 </script>
@@ -27,5 +31,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
+  margin-bottom: 0px;
 }
+
+/* temporary */
+body {
+  height: 400px;
+}
+
 </style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="footerBar">
+        <div id="footerContent">
+            <p>Some message... :)</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterBar'
+}
+</script>
+
+<style scoped>
+#footerBar {
+    width: auto;
+    padding: 10px;
+    background-color: lightcoral;
+}
+
+#footerContent {
+    text-align: center;
+    color: #42b983;
+}
+</style>
